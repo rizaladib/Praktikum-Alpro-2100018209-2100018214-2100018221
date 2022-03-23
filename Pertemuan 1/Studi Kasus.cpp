37c0e8b8334fc8400c01395326e4b28123c8b6a9
AdityaPrasetyoYogatama <101713148+AdityaPrasetyoYogatama@users.noreply.github.com>
2022-03-23 12:36:54 +0700

    Update and rename Studi Kasus to Studi Kasus.cpp

--- Pertemuan 1/Studi Kasus.cpp
+++ Pertemuan 1/Studi Kasus.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 int main(){
-  int kode,harga,jumlah,total,bayar,kembali;
+  int goreng,bakar,total,bayar;
   cout<<"~~~MENU KEDAI AYAM KAMI~~~"<<endl;
   cout<<"=========================="<<endl;
   cout<<"1. Ayam goreng Rp: 17.000"<<endl;
   cout<<"2. Ayam bakar Rp: 21.000"<<endl;
   
   cout<<"Jumlah Pesanan Ayam goreng : ";
+  cin>>goreng;
   cout<<"Jumlah Pesanan Ayam bakar : ";
-  cin>>kode;
+  cin>>bakar;
+  
+  total = (17000 * goreng) + (21000 * bakar);
+  if(total >= 45000){
+  	bayar = total - total * 10/100;
+  }
+  else{
+  	bayar = total;
+  }
+}
