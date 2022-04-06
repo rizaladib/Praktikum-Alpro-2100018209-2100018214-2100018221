411f73aedf1af11ba151ffea05ee49a7a62fd282
AdityaPrasetyoYogatama <101713148+AdityaPrasetyoYogatama@users.noreply.github.com>
2022-04-06 13:04:03 +0700

    Add files via upload

---
+++ Pertemuan 3/Studi Kasus/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include "library/input.h"
+#include "library/proses.h"
+#include "ibrary/output.h"
+using namespace std;
+
+int main(){
+  
+  return 0;
+}
